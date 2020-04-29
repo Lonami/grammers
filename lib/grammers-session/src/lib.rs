d1c027c44d208b7a232458906729bb14cd59109f
Lonami Exo <totufals@hotmail.com>
2020-04-29 20:11:20 +0200

    Simplify sessions to a minimum
    Should make it a lot easier to get started.

--- lib/grammers-session/src/lib.rs
+++ lib/grammers-session/src/lib.rs
@@ -5,20 +5,158 @@
 // <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
+use std::fs::{File, OpenOptions};
+use std::io::{self, BufRead, BufReader, Seek, Write};
+use std::net::SocketAddr;
+use std::path::Path;
 
-//! This crate provides the [`Session`] trait, which allows client instances
-//! to configure themselves with it.
-//!
-//! Sessions are used to persist the information required to connect to
-//! Telegram to disk, so that the expensive process of initializing the
-//! connection has only to be done once.
-//!
-//! [`Session`]: trait.session.html
+const CURRENT_VERSION: u32 = 1;
 
-mod memory_session;
-mod session;
-mod text_session;
+fn parse_hex(byte: &str) -> Option<u8> {
+    match u8::from_str_radix(byte, 16) {
+        Ok(x) => Some(x),
+        Err(_) => None,
+    }
+}
 
-pub use memory_session::MemorySession;
-pub use session::Session;
-pub use text_session::TextSession;
+fn key_from_hex(hex: &str) -> Option<[u8; 256]> {
+    let mut buffer = [0; 256];
+    if hex.len() == buffer.len() * 2 {
+        for (i, byte) in buffer.iter_mut().enumerate() {
+            let i = i * 2;
+            if let Some(value) = parse_hex(&hex[i..i + 2]) {
+                *byte = value;
+            } else {
+                return None;
+            }
+        }
+
+        Some(buffer)
+    } else {
+        None
+    }
+}
+
+fn hex_from_key(key: &[u8; 256]) -> String {
+    use std::fmt::Write;
+    let mut buffer = String::with_capacity(key.len() * 2);
+    for byte in key.iter() {
+        write!(buffer, "{:02x}", byte).unwrap();
+    }
+    buffer
+}
+
+pub struct Session {
+    file: File,
+    pub user_dc: Option<(i32, SocketAddr)>,
+    pub auth_key_data: Option<[u8; 256]>,
+}
+
+impl Session {
+    /// Loads or creates a new session file.
+    pub fn load_or_create<P: AsRef<Path>>(path: P) -> io::Result<Self> {
+        if let Ok(instance) = Self::load(path.as_ref()) {
+            Ok(instance)
+        } else {
+            Self::create(path)
+        }
+    }
+
+    /// Create a new session instance.
+    pub fn create<P: AsRef<Path>>(path: P) -> io::Result<Self> {
+        Ok(Self {
+            file: File::create(path)?,
+            user_dc: None,
+            auth_key_data: None,
+        })
+    }
+
+    /// Load a previous session instance.
+    pub fn load<P: AsRef<Path>>(path: P) -> io::Result<Self> {
+        let mut lines = BufReader::new(File::open(path.as_ref())?).lines();
+
+        // Version
+        let version: u32 = if let Some(Ok(line)) = lines.next() {
+            line.parse()
+                .map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "malformed session"))?
+        } else {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "malformed session",
+            ));
+        };
+        if version != CURRENT_VERSION {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "unknown version",
+            ));
+        }
+
+        // user_dc.0
+        let user_dc_id = if let Some(Ok(line)) = lines.next() {
+            match line.parse() {
+                Ok(x) => Some(x),
+                Err(_) => None,
+            }
+        } else {
+            None
+        };
+
+        // user_dc.1
+        let user_dc_addr = if let Some(Ok(line)) = lines.next() {
+            match line.parse() {
+                Ok(x) => Some(x),
+                Err(_) => None,
+            }
+        } else {
+            None
+        };
+
+        // user_dc
+        let user_dc = if let Some(id) = user_dc_id {
+            if let Some(addr) = user_dc_addr {
+                Some((id, addr))
+            } else {
+                None
+            }
+        } else {
+            None
+        };
+
+        // auth_key_data
+        let auth_key_data = if let Some(Ok(line)) = lines.next() {
+            key_from_hex(&line)
+        } else {
+            None
+        };
+
+        drop(lines);
+        Ok(Self {
+            file: OpenOptions::new().write(true).open(path.as_ref())?,
+            user_dc,
+            auth_key_data,
+        })
+    }
+
+    /// Saves the session file.
+    pub fn save(&mut self) -> io::Result<()> {
+        self.file.seek(io::SeekFrom::Start(0))?;
+        writeln!(self.file, "{}", CURRENT_VERSION)?;
+
+        if let Some((dc_id, dc_addr)) = self.user_dc {
+            writeln!(self.file, "{}", dc_id)?;
+            writeln!(self.file, "{}", dc_addr)?;
+        } else {
+            writeln!(self.file)?;
+            writeln!(self.file)?;
+        }
+
+        if let Some(data) = self.auth_key_data {
+            writeln!(self.file, "{}", hex_from_key(&data))?;
+        } else {
+            writeln!(self.file)?;
+        }
+        self.file.sync_data()?;
+        Ok(())
+    }
+}
