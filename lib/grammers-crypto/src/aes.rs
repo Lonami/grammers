e7d2f8d5e2385d35a0483358c13d843818bdfeba
Lonami Exo <totufals@hotmail.com>
2021-05-09 13:17:46 +0200

    Squeeze a little bit more performance in IGE methods

--- lib/grammers-crypto/src/aes.rs
+++ lib/grammers-crypto/src/aes.rs
@@ -11,24 +11,26 @@ use aes::cipher::{BlockDecrypt, BlockEncrypt, NewBlockCipher};
 /// Encrypt the input plaintext using the AES-IGE mode.
 pub fn ige_encrypt(plaintext: &[u8], key: &[u8; 32], iv: &[u8; 32]) -> Vec<u8> {
     assert!(plaintext.len() % 16 == 0);
-    let mut ciphertext = Vec::with_capacity(plaintext.len());
+    let mut ciphertext = vec![0; plaintext.len()];
+    assert!(ciphertext.len() % 16 == 0);
 
     let key = GenericArray::from_slice(key);
     let cipher = aes::Aes256::new(&key);
 
     let mut iv = iv.clone();
     let (iv1, iv2) = iv.split_at_mut(16);
+    assert!(iv1.len() == 16);
+    assert!(iv2.len() == 16);
 
-    for plaintext_block in plaintext.chunks(16) {
+    for (plaintext_block, ciphertext_block) in plaintext.chunks(16).zip(ciphertext.chunks_mut(16)) {
         // block = block XOR iv1
-        let mut xored_block = [0; 16];
-        xored_block
+        let ciphertext_block = GenericArray::from_mut_slice(ciphertext_block);
+        ciphertext_block
             .iter_mut()
             .zip(plaintext_block.iter().zip(iv1.iter()))
             .for_each(|(x, (a, b))| *x = a ^ b);
 
         // block = encrypt(block);
-        let ciphertext_block = GenericArray::from_mut_slice(&mut xored_block);
         cipher.encrypt_block(ciphertext_block);
 
         // block = block XOR iv2
@@ -38,7 +40,6 @@ pub fn ige_encrypt(plaintext: &[u8], key: &[u8; 32], iv: &[u8; 32]) -> Vec<u8> {
             .for_each(|(x, a)| *x ^= a);
 
         // save ciphertext and adjust iv
-        ciphertext.extend(ciphertext_block.iter());
         iv1.copy_from_slice(ciphertext_block);
         iv2.copy_from_slice(plaintext_block);
     }
@@ -49,24 +50,26 @@ pub fn ige_encrypt(plaintext: &[u8], key: &[u8; 32], iv: &[u8; 32]) -> Vec<u8> {
 /// Decrypt the input ciphertext using the AES-IGE mode.
 pub fn ige_decrypt(ciphertext: &[u8], key: &[u8; 32], iv: &[u8; 32]) -> Vec<u8> {
     assert!(ciphertext.len() % 16 == 0);
-    let mut plaintext = Vec::with_capacity(ciphertext.len());
+    let mut plaintext = vec![0; ciphertext.len()];
+    assert!(plaintext.len() % 16 == 0);
 
     let key = GenericArray::from_slice(key);
     let cipher = aes::Aes256::new(&key);
 
     let mut iv = iv.clone();
     let (iv1, iv2) = iv.split_at_mut(16);
+    assert!(iv1.len() == 16);
+    assert!(iv2.len() == 16);
 
-    for ciphertext_block in ciphertext.chunks(16) {
+    for (ciphertext_block, plaintext_block) in ciphertext.chunks(16).zip(plaintext.chunks_mut(16)) {
         // block = block XOR iv2
-        let mut xored_block = [0; 16];
-        xored_block
+        let plaintext_block = GenericArray::from_mut_slice(plaintext_block);
+        plaintext_block
             .iter_mut()
             .zip(ciphertext_block.iter().zip(iv2.iter()))
             .for_each(|(x, (a, b))| *x = a ^ b);
 
         // block = decrypt(block);
-        let plaintext_block = GenericArray::from_mut_slice(&mut xored_block);
         cipher.decrypt_block(plaintext_block);
 
         // block = block XOR iv1
@@ -76,7 +79,6 @@ pub fn ige_decrypt(ciphertext: &[u8], key: &[u8; 32], iv: &[u8; 32]) -> Vec<u8> 
             .for_each(|(x, a)| *x ^= a);
 
         // save plaintext and adjust iv
-        plaintext.extend(plaintext_block.iter());
         iv1.clone_from_slice(ciphertext_block);
         iv2.clone_from_slice(plaintext_block);
     }
