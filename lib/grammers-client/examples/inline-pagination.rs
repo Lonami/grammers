72f4b04c385506df06051d7b1171c4479878a7c2
Aykut Yilmaz <aykuxt@gmail.com>
2021-05-23 22:26:06 +0200

    update examples

--- lib/grammers-client/examples/inline-pagination.rs
+++ lib/grammers-client/examples/inline-pagination.rs
@@ -64,7 +64,7 @@ fn fib_markup(mut a: u128, mut b: u128) -> reply_markup::Inline {
 
 async fn handle_update(_client: Client, update: Update) -> Result {
     match update {
-        Update::NewMessage(mut message) if message.text() == "/start" => {
+        Update::NewMessage(message) if message.text() == "/start" => {
             message
                 .respond(InputMessage::text("Here's a fibonacci").reply_markup(&fib_markup(0, 1)))
                 .await?;
