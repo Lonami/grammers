762026f7a334414186118762979cd39f112ebe4f
Lonami Exo <totufals@hotmail.com>
2021-06-05 20:01:21 +0200

    Add missing license header

--- lib/grammers-client/src/types/inline_query.rs
+++ lib/grammers-client/src/types/inline_query.rs
@@ -1,3 +1,10 @@
+// Copyright 2020 - developers of the `grammers` project.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
 use super::{Chat, ChatMap, User};
 use crate::{client::Client, utils::generate_random_id, InputMessage};
 use grammers_mtsender::InvocationError;
