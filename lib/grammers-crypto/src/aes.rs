815af55969b50813a70adb56b344af5279ea63f0
rupansh-arch <rupanshsekar@hotmail.com>
2021-10-12 23:57:54 +0530

    crypto: aes: use iter
    simd opts fixed with rust 1.55

--- lib/grammers-crypto/src/aes.rs
+++ lib/grammers-crypto/src/aes.rs
@@ -18,24 +18,24 @@ pub fn ige_encrypt(plaintext: &[u8], key: &[u8; 32], iv: &[u8; 32]) -> Vec<u8> {
 
     let mut iv = *iv;
     let (iv1, iv2) = iv.split_at_mut(16);
-    assert!(iv1.len() == 16);
-    assert!(iv2.len() == 16);
 
     for (plaintext_block, ciphertext_block) in plaintext.chunks(16).zip(ciphertext.chunks_mut(16)) {
-        assert!(plaintext_block.len() == 16);
         // block = block XOR iv1
-        for i in 0..ciphertext_block.len() {
-            ciphertext_block[i] = plaintext_block[i] ^ iv1[i];
-        }
+        ciphertext_block
+            .iter_mut()
+            .zip(plaintext_block)
+            .zip(iv1.as_ref())
+            .for_each(|((x, a), b)| *x = a ^ b);
 
         // block = encrypt(block);
         let ciphertext_block = GenericArray::from_mut_slice(ciphertext_block);
         cipher.encrypt_block(ciphertext_block);
 
         // block = block XOR iv2
-        for i in 0..ciphertext_block.len() {
-            ciphertext_block[i] ^= iv2[i];
-        }
+        ciphertext_block
+            .iter_mut()
+            .zip(iv2.as_ref())
+            .for_each(|(x, a)| *x ^= a);
 
         // save ciphertext and adjust iv
         iv1.copy_from_slice(ciphertext_block);
@@ -54,25 +54,29 @@ pub fn ige_decrypt(ciphertext: &[u8], key: &[u8; 32], iv: &[u8; 32]) -> Vec<u8> 
     let key = GenericArray::from_slice(key);
     let cipher = aes::Aes256::new(&key);
     let mut iv = *iv;
+    let (iv1, iv2) = iv.split_at_mut(16);
 
     for (ciphertext_block, plaintext_block) in ciphertext.chunks(16).zip(plaintext.chunks_mut(16)) {
-        let plaintext_block = GenericArray::from_mut_slice(plaintext_block);
         // block = block XOR iv2
-        for i in 0..16 {
-            plaintext_block[i] = ciphertext_block[i] ^ iv[i + 16];
-        }
+        plaintext_block
+            .iter_mut()
+            .zip(ciphertext_block)
+            .zip(iv1.as_ref())
+            .for_each(|((a, x), b)| *a = x ^ b);
 
         // block = decrypt(block);
+        let plaintext_block = GenericArray::from_mut_slice(plaintext_block);
         cipher.decrypt_block(plaintext_block);
 
         // block = block XOR iv1
-        for i in 0..16 {
-            plaintext_block[i] ^= iv[i];
-        }
+        plaintext_block
+            .iter_mut()
+            .zip(iv2.as_ref())
+            .for_each(|(a, b)| *a ^= b);
 
         // save plaintext and adjust iv
-        iv[0..16].copy_from_slice(ciphertext_block);
-        iv[16..32].copy_from_slice(plaintext_block);
+        iv1.copy_from_slice(ciphertext_block);
+        iv2.copy_from_slice(plaintext_block);
     }
 
     plaintext
