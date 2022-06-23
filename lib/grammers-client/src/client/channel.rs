fd42d01f933a9c7d6cb54c10bc22bd4df4231d78
tingfeng-key <1770963469@qq.com>
2022-06-23 23:32:57 +0800

    optimize channel.rs file code

--- lib/grammers-client/src/client/channel.rs
+++ lib/grammers-client/src/client/channel.rs
@@ -3,9 +3,10 @@ use grammers_mtsender::InvocationError;
 use grammers_session::PackedChat;
 use grammers_tl_types as tl;
 
-impl Client {
-    const MAX_PARTICIPANT_LIMIT: usize = 200;
+const MAX_PARTICIPANT_LIMIT: i32 = 200;
 
+impl Client {
+    //get full channel
     pub async fn get_full_channel<C: Into<PackedChat>>(
         &mut self,
         chat: C,
@@ -24,6 +25,7 @@ impl Client {
         Ok(chat_full)
     }
 
+    // get chat' members
     pub async fn get_chat_members<C: Into<PackedChat>>(
         &self,
         chat: C,
@@ -39,30 +41,29 @@ impl Client {
             channel: tl::enums::InputChannel::Channel(input_channel),
             filter,
             offset: 0,
-            limit: Self::MAX_PARTICIPANT_LIMIT as i32,
+            limit: MAX_PARTICIPANT_LIMIT,
             hash: 0,
         };
         let mut chat_members: Vec<tl::enums::User> = vec![];
         loop {
-            let res = self.invoke(&request).await?;
-            match res {
-                tl::enums::channels::ChannelParticipants::Participants(p) => {
-                    for elem in p.users {
-                        chat_members.push(elem);
-                    }
-                    if request.offset >= p.count {
-                        break;
-                    } else {
-                        if request.limit >= p.count {
-                            break;
-                        } else if (request.offset + request.limit) >= p.count {
-                            break;
-                        } else {
-                            request.offset += request.limit;
-                        }
-                    }
+            if let tl::enums::channels::ChannelParticipants::Participants(p) =
+                self.invoke(&request).await?
+            {
+                for elem in p.users {
+                    chat_members.push(elem);
                 }
-                tl::enums::channels::ChannelParticipants::NotModified => {}
+                if request.offset >= p.count {
+                    break;
+                }
+
+                if request.limit >= p.count {
+                    break;
+                }
+
+                if (request.offset + request.limit) >= p.count {
+                    break;
+                }
+                request.offset += request.limit;
             }
         }
         Ok(chat_members)
