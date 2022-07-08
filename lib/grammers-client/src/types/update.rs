67a9f84d3fea235f07238a2d02aebad0b5ac8a40
George Miao <gm@miao.dev>
2022-07-09 00:30:57 +0700

    Add Edited, Deleted and Raw variants to Update (#103)

--- lib/grammers-client/src/types/update.rs
+++ lib/grammers-client/src/types/update.rs
@@ -5,22 +5,36 @@
 // <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
-use super::{CallbackQuery, ChatMap, InlineQuery, Message};
-use crate::Client;
-use grammers_tl_types as tl;
 use std::sync::Arc;
+
+use grammers_tl_types as tl;
+
+use super::{CallbackQuery, ChatMap, InlineQuery, Message};
+use crate::{types::MessageDeletion, Client};
 
 #[non_exhaustive]
 #[derive(Debug, Clone)]
 pub enum Update {
     /// Occurs whenever a new text message or a message with media is produced.
     NewMessage(Message),
-    /// Occurs when Telegram calls back into your bot because an inline callback button was
-    /// pressed.
+    /// Occurs when a message is updated.
+    MessageEdited(Message),
+    /// Occurs when a message is deleted.
+    MessageDeleted(MessageDeletion),
+    /// Occurs when Telegram calls back into your bot because an inline callback
+    /// button was pressed.
     CallbackQuery(CallbackQuery),
-    /// Occurs whenever you sign in as a bot and a user sends an inline query such as
-    /// `@bot query`.
+    /// Occurs whenever you sign in as a bot and a user sends an inline query
+    /// such as `@bot query`.
     InlineQuery(InlineQuery),
+    /// Raw events are not actual events.
+    /// Instead, they are the raw Update object that Telegram sends. You
+    /// normally shouldn’t need these.
+    ///
+    /// **NOTE**: the library can split raw updates into actual `Update`
+    /// variants so use this only as the workaround when such variant is not
+    /// available yet.
+    Raw(tl::enums::Update),
 }
 
 impl Update {
@@ -33,6 +47,19 @@ impl Update {
             tl::enums::Update::NewMessage(tl::types::UpdateNewMessage { message, .. }) => {
                 Message::new(client, message, chats).map(Self::NewMessage)
             }
+            tl::enums::Update::EditMessage(tl::types::UpdateEditMessage { message, .. }) => {
+                Message::new(client, message, chats).map(Self::MessageEdited)
+            }
+            tl::enums::Update::DeleteMessages(tl::types::UpdateDeleteMessages {
+                messages, ..
+            }) => Some(Self::MessageDeleted(MessageDeletion::new(messages))),
+            tl::enums::Update::DeleteChannelMessages(tl::types::UpdateDeleteChannelMessages {
+                messages,
+                channel_id,
+                ..
+            }) => Some(Self::MessageDeleted(MessageDeletion::new_with_channel(
+                messages, channel_id,
+            ))),
             tl::enums::Update::NewChannelMessage(tl::types::UpdateNewChannelMessage {
                 message,
                 ..
@@ -43,7 +70,7 @@ impl Update {
             tl::enums::Update::BotInlineQuery(query) => {
                 Some(Self::InlineQuery(InlineQuery::new(client, query, chats)))
             }
-            _ => None,
+            update => Some(Self::Raw(update)),
         }
     }
 }
