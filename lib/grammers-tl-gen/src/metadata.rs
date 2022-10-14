debd530d8addbe2d581eae8bb531d50db818bd25
Lonami Exo <totufals@hotmail.com>
2022-10-14 16:07:13 +0200

    Handle indirect recursion in the generator

--- lib/grammers-tl-gen/src/metadata.rs
+++ lib/grammers-tl-gen/src/metadata.rs
@@ -22,31 +22,30 @@ impl<'a> Metadata<'a> {
             defs_with_type: HashMap::new(),
         };
 
-        definitions
+        let type_definitions = definitions
             .iter()
             .filter(|d| d.category == Category::Types)
-            .for_each(|d| {
-                if d.params.iter().any(|p| match &p.ty {
-                    ParameterType::Flags => false,
-                    ParameterType::Normal { ty, .. } => {
-                        ty.namespace == d.ty.namespace && ty.name == d.ty.name
-                    }
-                }) {
-                    metadata.recursing_defs.insert(d.id);
-                }
+            .collect::<Vec<_>>();
 
-                metadata
-                    .defs_with_type
-                    .entry((&d.ty.namespace, &d.ty.name))
-                    .or_insert_with(Vec::new)
-                    .push(d);
-            });
+        type_definitions.iter().for_each(|d| {
+            metadata
+                .defs_with_type
+                .entry((&d.ty.namespace, &d.ty.name))
+                .or_insert_with(Vec::new)
+                .push(d);
+        });
+
+        type_definitions.iter().for_each(|d| {
+            if def_self_references(d, d, &metadata.defs_with_type, &mut HashSet::new()) {
+                metadata.recursing_defs.insert(d.id);
+            }
+        });
 
         metadata
     }
 
-    /// Returns `true` if any of the parameters of `Definition` are of the
-    /// same type as the `Definition` itself (meaning it recurses).
+    /// Returns `true` if any of the parameters of `Definition` eventually
+    /// contains the same type as the `Definition` itself (meaning it recurses).
     pub fn is_recursive_def(&self, def: &Definition) -> bool {
         self.recursing_defs.contains(&def.id)
     }
@@ -54,4 +53,36 @@ impl<'a> Metadata<'a> {
     pub fn defs_with_type(&self, ty: &'a Type) -> &Vec<&Definition> {
         &self.defs_with_type[&(&ty.namespace, &ty.name)]
     }
+}
+
+fn def_self_references(
+    root: &Definition,
+    check: &Definition,
+    defs_with_type: &HashMap<(&Vec<String>, &String), Vec<&Definition>>,
+    visited: &mut HashSet<u32>,
+) -> bool {
+    visited.insert(check.id);
+    for param in check.params.iter() {
+        match &param.ty {
+            ParameterType::Flags => {}
+            ParameterType::Normal { ty, .. } => {
+                if ty.namespace == root.ty.namespace && ty.name == root.ty.name {
+                    return true;
+                }
+
+                if let Some(defs) = defs_with_type.get(&(&ty.namespace, &ty.name)) {
+                    for def in defs {
+                        if visited.contains(&def.id) {
+                            continue;
+                        }
+                        if def_self_references(root, def, defs_with_type, visited) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    false
 }
--- lib/grammers-tl-gen/tests/lib.rs
+++ lib/grammers-tl-gen/tests/lib.rs
@@ -19,14 +19,7 @@ fn get_definitions(contents: &str) -> Vec<Definition> {
         .collect()
 }
 
-#[test]
-fn generic_functions_use_generic_parameters() -> io::Result<()> {
-    let definitions = get_definitions(
-        "
-        ---functions---
-        invokeWithLayer#da9b0d0d {X:Type} layer:int query:!X = X;
-    ",
-    );
+fn gen_rust_code(definitions: &[Definition]) -> io::Result<String> {
     let mut file = Vec::new();
     generate_rust_code(
         &mut file,
@@ -40,7 +33,18 @@ fn generic_functions_use_generic_parameters() -> io::Result<()> {
             impl_from_type: true,
         },
     )?;
-    let result = String::from_utf8(file).unwrap();
+    Ok(String::from_utf8(file).unwrap())
+}
+
+#[test]
+fn generic_functions_use_generic_parameters() -> io::Result<()> {
+    let definitions = get_definitions(
+        "
+        ---functions---
+        invokeWithLayer#da9b0d0d {X:Type} layer:int query:!X = X;
+    ",
+    );
+    let result = gen_rust_code(&definitions)?;
     eprintln!("{}", result);
     assert!(result.contains("pub struct InvokeWithLayer<X>"));
     assert!(result.contains("pub query: X,"));
@@ -52,5 +56,69 @@ fn generic_functions_use_generic_parameters() -> io::Result<()> {
         .contains("impl<X: crate::Deserializable> crate::Deserializable for InvokeWithLayer<X>"));
     assert!(result.contains("impl<X: crate::RemoteCall> crate::RemoteCall for InvokeWithLayer<X>"));
     assert!(result.contains("type Return = X::Return;"));
+    Ok(())
+}
+
+#[test]
+fn recursive_types_direct_boxed() -> io::Result<()> {
+    let definitions = get_definitions(
+        "
+        textBold#6724abc4 text:RichText = RichText;
+    ",
+    );
+    let result = gen_rust_code(&definitions)?;
+    eprintln!("{}", result);
+    assert!(result.contains("TextBold(Box<crate::types::TextBold>)"));
+    assert!(result.contains("RichText::TextBold(Box::new("));
+    assert!(result.contains("Self::TextBold(Box::new("));
+    Ok(())
+}
+
+#[test]
+fn recursive_types_indirect_boxed() -> io::Result<()> {
+    let definitions = get_definitions(
+        "
+        messageExtendedMedia#ee479c64 media:MessageMedia = MessageExtendedMedia;
+        messageMediaInvoice#f6a548d3 flags:# extended_media:flags.4?MessageExtendedMedia = MessageMedia;
+    ",
+    );
+    let result = gen_rust_code(&definitions)?;
+    eprintln!("{}", result);
+    assert!(result.contains("Media(Box<crate::types::MessageExtendedMedia>),"));
+    assert!(result.contains("Box::new(crate::types::MessageExtendedMedia::deserialize("));
+    assert!(result.contains("MessageExtendedMedia::Media(Box::new("));
+    assert!(result.contains("Invoice(Box<crate::types::MessageMediaInvoice>),"));
+    assert!(result.contains("Box::new(crate::types::MessageMediaInvoice::deserialize("));
+    assert!(result.contains("MessageMedia::Invoice(Box::new("));
+    Ok(())
+}
+
+#[test]
+fn recursive_types_indirect_no_hang() -> io::Result<()> {
+    let definitions = get_definitions(
+        "
+        inputUserFromMessage#1da448e2 peer:InputPeer msg_id:int user_id:long = InputUser;
+        inputPeerUserFromMessage#a87b0a1c peer:InputPeer msg_id:int user_id:long = InputPeer;
+    ",
+    );
+    let _ = gen_rust_code(&definitions)?;
+    Ok(())
+}
+
+#[test]
+fn recursive_types_vec_indirect_not_boxed() -> io::Result<()> {
+    let definitions = get_definitions(
+        "
+        jsonObjectValue#c0de1bd9 key:string value:JSONValue = JSONObjectValue;
+
+        jsonArray#f7444763 value:Vector<JSONValue> = JSONValue;
+        jsonObject#99c1d49d value:Vector<JSONObjectValue> = JSONValue;
+    ",
+    );
+    let result = gen_rust_code(&definitions)?;
+    eprintln!("{}", result);
+    assert!(result.contains("JsonObjectValue(crate::types::JsonObjectValue)"));
+    assert!(result.contains("JsonArray(crate::types::JsonArray)"));
+    assert!(result.contains("JsonObject(crate::types::JsonObject)"));
     Ok(())
 }
