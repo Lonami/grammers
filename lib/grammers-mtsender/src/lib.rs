8665dae0532d6da22818f17bea5b38b2da02f492
Lonami Exo <totufals@hotmail.com>
2023-08-31 01:14:18 +0200

    Avoid trashing of mtsender mtp_buffer on write

--- lib/grammers-mtsender/src/lib.rs
+++ lib/grammers-mtsender/src/lib.rs
@@ -415,11 +415,9 @@ impl<T: Transport, M: Mtp> Sender<T, M> {
                 break;
             }
         }
-        let temp_vec = self.mtp.finalize();
-        self.mtp_buffer = temp_vec[..].into();
+        let mtp_buffer = self.mtp.finalize();
         self.write_buffer.clear();
-        self.transport
-            .pack(&self.mtp_buffer, &mut self.write_buffer);
+        self.transport.pack(&mtp_buffer, &mut self.write_buffer);
 
         // NOTE: we have to use the FILTERED requests, not the saved ones.
         // The key to finding this was printing the old and new state (but took ~2h to find).
