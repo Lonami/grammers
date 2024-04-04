8ee1db01dd1b6f0d61499939abd525c89e0576dd
Lonami Exo <totufals@hotmail.com>
2024-04-04 23:06:15 +0200

    Add missing license header

--- lib/grammers-client/src/types/message_deletion.rs
+++ lib/grammers-client/src/types/message_deletion.rs
@@ -1,3 +1,11 @@
+// Copyright 2020 - developers of the `grammers` project.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 /// Occurs whenever a message is deleted.
 /// Note that this event isn’t 100% reliable, since Telegram doesn’t always
 /// notify the clients that a message was deleted.
