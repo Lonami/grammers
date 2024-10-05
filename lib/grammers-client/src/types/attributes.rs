c4837a2efcc63f1cf396e96d561f012c24033758
- <5144598+-@users.noreply.github.com>
2024-10-05 19:22:48 +0300

    derive `Clone` for `InputMessage` and `Attribute`

--- lib/grammers-client/src/types/attributes.rs
+++ lib/grammers-client/src/types/attributes.rs
@@ -8,6 +8,7 @@
 use grammers_tl_types as tl;
 use std::{convert::TryInto, time::Duration};
 
+#[derive(Clone)]
 pub enum Attribute {
     Audio {
         duration: Duration,
--- lib/grammers-client/src/types/input_message.rs
+++ lib/grammers-client/src/types/input_message.rs
@@ -14,7 +14,7 @@ use std::time::{SystemTime, UNIX_EPOCH};
 const SCHEDULE_ONCE_ONLINE: i32 = 0x7ffffffe;
 
 /// Construct and send rich text messages with various options.
-#[derive(Default)]
+#[derive(Clone, Default)]
 pub struct InputMessage {
     pub(crate) background: bool,
     pub(crate) clear_draft: bool,
