0cb95526191b0f625b52bd03110753afdbbf10f3
Dusk Banks <me@bb010g.com>
2025-11-09 06:02:45 -0800

    reflow some comments to diff cleaner on rewrites

--- grammers-session/src/peer.rs
+++ grammers-session/src/peer.rs
@@ -109,12 +109,14 @@ pub enum ChannelKind {
     Gigagroup,
 }
 
-/// Sentinel value used to represent the self-user when its true `PeerId` is unknown.
+/// Sentinel value used to represent the self-user
+/// when its true `PeerId` is unknown.
 ///
 /// Per https://core.telegram.org/api/bots/ids:
 /// > a bot API dialog ID ranges from -4000000000000 to 1099511627775
 ///
-/// This value is not intended to be visible or persisted, so it can be changed as needed in the future.
+/// This value is not intended to be visible or persisted,
+/// so it can be changed as needed in the future.
 const SELF_USER_ID: PeerId = PeerId(1 << 40);
 
 /// Sentinel value used to represent empty chats.
@@ -124,10 +126,12 @@ const SELF_USER_ID: PeerId = PeerId(1 << 40);
 /// >
 /// > \[…] transformed range for bot API channel dialog IDs is -1997852516352 to -1000000000001 inclusively
 ///
-/// `chat_id` parameters are in Telegram's API use the bare identifier, so there's no
-/// empty constructor, but it can be mimicked by picking the value in the correct range hole.
-/// This value is closer to "channel with ID 0" than "chat with ID 0", but there's no distinct
-/// `-0` integer, and channels have a proper constructor for empty already
+/// `chat_id` parameters are in Telegram's API use the bare identifier,
+/// so there's no empty constructor,
+/// but it can be mimicked by picking the value in the correct range hole.
+/// This value is closer to "channel with ID 0" than "chat with ID 0",
+/// but there's no distinct `-0` integer,
+/// and channels have a proper constructor for empty already
 const EMPTY_CHAT_ID: i64 = -1000000000000;
 
 impl PeerId {
